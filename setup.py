8f7c1631e36bc835a94d75b6b52de4954974753d
Aeva Ntsc <aeva.ntsc@gmail.com>
2013-07-16 16:04:06 -0500

    Added setup.py and project module.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="hoodwink",
+      version="zero",
+      description="",
+      url="https://github.com/Aeva/hoodwink-serial",
+      author="Aeva Palecek",
+      author_email="aeva.ntsc@gmail.com",
+      license="GPLv3",
+      packages=["serial"],
+      zip_safe=False,
+
+      ])
